85287700ce2a827f9a75057f779d0eacfc1e58ba
Dima Kamalov <dima.kamalov@gmail.com>
2013-07-07 22:13:24 -0700

    Replacing dateutil.parser with custom function
    Previously dateutil.parser was a bottleneck in our data processing.  The
    time in the input spreadsheet is in a single format with no timezone
    changes so we can process the time faster manually.
    
    The input reading runtime went from ~150 sec to ~10 sec with this change.

--- datareader.py
+++ datareader.py
@@ -6,7 +6,6 @@ Created on Sun Jul 07 15:56:56 2013
 """
 
 import csv
-import dateutil.parser
 import onedrive
 
 def read_gps(gpsstr):
@@ -14,6 +13,18 @@ def read_gps(gpsstr):
     input gpsstr is [latstr, lonstr]
     """
     return onedrive.Gps(float(gpsstr[0]), float(gpsstr[1]))
+    
+def read_time(timestr):
+    """
+    this converts the time string in the input data to seconds since 
+    0:00:00 1/1/2007 utc.  
+    note that the time is not real;  see gpsdata/readme.txt for details
+    
+    we do this hack instead of using dateutil.parser because we don't need
+    to allow for multiple formats/timezones and dateutil.parser is very slow.
+    """
+    return (int(timestr[8:10])-1)*86400+int(timestr[11:13])*3600+ \
+           int(timestr[14:16])*60+int(timestr[17:19])
 
 def read_data(datafile):
     """
@@ -26,7 +37,7 @@ def read_data(datafile):
     
     for row in everything:
         drive_id = int(row[0])
-        datetime = dateutil.parser.parse(row[1])
+        datetime = read_time(row[1])
         gps = read_gps(row[2:4])
         
         if drive_id in drives:
